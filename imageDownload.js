745394c49b29b40e72155b3dcee0bcf52aa81521
donge <1114069497@qq.com>
2024-02-21 17:49:31 +0800

    Create imageDownload.js

---
+++ imageDownload.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         图片下载
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  图片下载
+// @author       zhuiyi
+// @match        https://*/*
+// @match        http://*/*
+// @icon         none
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+})();
